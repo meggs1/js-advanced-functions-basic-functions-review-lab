7aff677a85d50be3eeba9bb751c276019788e50a
megan <megan.schertz@gmail.com>
2021-05-20 14:16:11 -0500

    commit tree 2a0195b7292e9f6cc0471f8534729cdaa7e6ff00

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair="*") {
+    return function(adjective="special") {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
+
+const Calculator = {
+    add(num1, num2) {return num1 + num2},
+    subtract(num1, num2) {return num1 - num2},
+    multiply(num1, num2) {return num1 * num2},
+    divide(num1, num2) {return num1 / num2}
+}
+
+function actionApplyer(startInt, arrayOfFunctions) {
+    let a = startInt
+
+    for (let i = 0; i < arrayOfFunctions.length; i++){
+        a = arrayOfFunctions[i](a)
+    }
+    return a
+}
